6181a778ba50fb03b3ef0b05558fb84fd29e01d1
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-14 19:34:31 +0530

    config🔧: Add jest config and tests  file structure

---
+++ tests/jest.config.js
@@ -0,0 +1,10 @@
+/** @format */
+
+module.exports = {
+  roots: ["<rootDir>/tests"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
