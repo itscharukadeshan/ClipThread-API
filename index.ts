4c42eaa4ce818ca085a44232873eda2346e143b0
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-15 11:06:15 +0530

    refactor🎨: Remove passport js dependent code

--- index.ts
+++ index.ts
@@ -11,51 +11,9 @@ import {
   YOUTUBE_CLIENT_SECRET,
 } from "./config/config";
 
-import { prisma } from "./prisma/utils/client";
-const { User } = require("./prisma/models/User");
-
-import passport from "passport";
-import { Strategy as TwitchStrategy } from "passport-twitch-latest";
-import { OAuth2Strategy as YouTubeStrategy } from "passport-google-oauth";
-
 import authRoutes from "./routes/auth.routes";
 
 const app: express.Application = express();
-
-passport.use(
-  new TwitchStrategy(
-    {
-      clientID: TWITCH_CLIENT_ID as string,
-      clientSecret: TWITCH_CLIENT_SECRET as string,
-      callbackURL: "/auth/twitch/callback",
-    },
-    verifyCallback
-  )
-);
-
-passport.use(
-  new YouTubeStrategy(
-    {
-      clientID: YOUTUBE_CLIENT_ID as string,
-      clientSecret: YOUTUBE_CLIENT_SECRET as string,
-      callbackURL: "/auth/youtube/callback",
-    },
-    verifyCallback
-  )
-);
-
-// Auth routes
-app.use("/auth", authRoutes);
-
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ profile }, function (err, user) {
-    if (err) {
-      return done(err);
-    }
-
-    done(null, user);
-  });
-}
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Welcome to clip thread api`);
