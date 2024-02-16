5b3d2829ee92c5f27de2f61a5251b92fc0959d9e
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-16 12:10:22 +0530

    format🥚: Remove unwanted

--- index.ts
+++ index.ts
@@ -2,17 +2,6 @@
 
 import express, { Request, Response } from "express";
 
-import axios, { AxiosResponse } from "axios";
-
-import {
-  TWITCH_CLIENT_ID,
-  TWITCH_CLIENT_SECRET,
-  YOUTUBE_CLIENT_ID,
-  YOUTUBE_CLIENT_SECRET,
-} from "./config/config";
-
-import authRoutes from "./routes/auth.routes";
-
 const app: express.Application = express();
 
 app.get("/", (req: Request, res: Response) => {
