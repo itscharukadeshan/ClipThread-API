9e3dfe2cfec5a7e9fd6f27a0d50696ae3d2e296a
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-16 14:08:45 +0530

    format🥚: Add missing types and add redirect uri

--- config/config.ts
+++ config/config.ts
@@ -4,14 +4,16 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
-const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
-const YOUTUBE_CLIENT_ID = process.env.YOUTUBE_CLIENT_ID;
-const YOUTUBE_CLIENT_SECRET = process.env.YOUTUBE_CLIENT_SECRET;
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID as string;
+const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET as string;
+const YOUTUBE_CLIENT_ID = process.env.YOUTUBE_CLIENT_ID as string;
+const YOUTUBE_CLIENT_SECRET = process.env.YOUTUBE_CLIENT_SECRET as string;
+const TWITCH_REDIRECT_URI = process.env.TWITCH_REDIRECT_URI as string;
 
 export {
   TWITCH_CLIENT_ID,
   TWITCH_CLIENT_SECRET,
   YOUTUBE_CLIENT_ID,
   YOUTUBE_CLIENT_SECRET,
+  TWITCH_REDIRECT_URI,
 };
