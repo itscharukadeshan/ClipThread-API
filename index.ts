b55274b05e117b69131a9b22540d8877b6dea36d
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-16 14:08:01 +0530

    feat✨: Add /twitch route

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 /** @format */
 
 import express, { Request, Response } from "express";
+import twitchRoutes from "./routes/twitch/twitch.routes";
 
 const app: express.Application = express();
 
@@ -9,6 +10,8 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 const port = 3000;
+
+app.use("/twitch", twitchRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
