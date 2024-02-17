d48b6885dd07b91bef4a7973eb6d954e9f3f3e80
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-17 12:03:53 +0530

    config🔧: Add SECRET_IV , ENCRYPTION_METHOD for the config

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,6 @@
 /** @format */
 
 import * as dotenv from "dotenv";
-import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -11,6 +10,24 @@ const YOUTUBE_CLIENT_ID = process.env.YOUTUBE_CLIENT_ID as string;
 const YOUTUBE_CLIENT_SECRET = process.env.YOUTUBE_CLIENT_SECRET as string;
 const TWITCH_REDIRECT_URI = process.env.TWITCH_REDIRECT_URI as string;
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY as string;
+const SECRET_IV = process.env.SECRET_IV as string;
+const ENCRYPTION_METHOD = process.env.ENCRYPTION_METHOD as string;
+
+/*
+console.log(`SECRET_IV length = ${SECRET_IV.length} || Should be 16 `);
+console.log(
+  `Encryption key length = ${ENCRYPTION_KEY.length} || Should be 32 `
+);
+*/
+
+// Use this to build strong SECRET_IV
+// ENCRYPTION_KEY should be 32 bit
+
+/*
+const crypto = require("crypto");
+const iv = crypto.randomBytes(16);
+console.log(iv.toString("hex"));
+*/
 
 export {
   TWITCH_CLIENT_ID,
@@ -19,4 +36,6 @@ export {
   YOUTUBE_CLIENT_SECRET,
   TWITCH_REDIRECT_URI,
   ENCRYPTION_KEY,
+  ENCRYPTION_METHOD,
+  SECRET_IV,
 };
