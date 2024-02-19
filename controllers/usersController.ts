5f1ae8d7b211e3b3e0caf7214357ef743de3ad18
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-19 16:56:40 +0530

    feat✨: Add createUser ( ) function

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -1,10 +1,29 @@
 const { PrismaClient } = require("@prisma/client");
+const { User } = require("../types");
+
 const prisma = new PrismaClient();
 
-async function createUser(userData: {}) {
+async function createUser(userData: Omit<User, "id">): Promise<User> {
   try {
+    const existingUsers = await prisma.user.findMany({
+      where: {
+        OR: [
+          { twitchId: userData.twitchId },
+          { youtubeId: userData.youtubeId },
+        ],
+      },
+    });
+
+    if (existingUsers.length > 0) {
+      throw new Error(`User with same Twitch ID or YouTube ID already exists`);
+    }
+
+    const userDataWithType = {
+      ...userData,
+    };
+
     const newUser = await prisma.user.create({
-      data: userData,
+      data: userDataWithType,
     });
     return newUser;
   } catch (error) {
