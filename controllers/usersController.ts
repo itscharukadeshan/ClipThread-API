7a0e4504b0e8f5e690201db5e02aa75e789e54f4
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-21 13:39:08 +0530

    feat✨: Update the create user function to handle both twitch and youtube auth

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -1,15 +1,55 @@
 const { PrismaClient } = require("@prisma/client");
 import { User } from "../prisma/client";
+import {
+  UserWithoutId,
+  TwitchAuthWithoutId,
+  YoutubeAuthWithoutId,
+} from "./types";
 
 const prisma = new PrismaClient();
-export async function createUser(userData: User): Promise<User> {
+export async function createUser(
+  userData: UserWithoutId,
+  twitchAuth?: TwitchAuthWithoutId,
+  youtubeAuth?: YoutubeAuthWithoutId
+): Promise<User> {
   try {
-    const newUser = await prisma.user.create({
+    const user = await prisma.user.create({
       data: userData,
     });
+
+    if (twitchAuth && youtubeAuth) {
+      throw new Error(
+        "Something went wrong please try again : Missing authCredentials"
+      );
+    }
+
+    if (twitchAuth) {
+      await prisma.twitchAuth.create({
+        data: {
+          user: { connect: { id: user.id } },
+          ...twitchAuth,
+        },
+      });
+    }
+
+    if (youtubeAuth) {
+      await prisma.youtubeAuth.create({
+        data: {
+          user: { connect: { id: user.id } },
+          ...youtubeAuth,
+        },
+      });
+    }
+
+    const newUser = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+
     return newUser;
-  } catch (error: any) {
-    throw new Error(`Failed to create user:${error}`);
+  } catch (error) {
+    throw new Error(`Failed to create user: ${error}`);
   }
 }
 
