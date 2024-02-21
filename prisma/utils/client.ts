2965c58e13b8104da64fad1223d65b2955d346e9
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-21 10:42:57 +0530

    config🔧: Update the prisma client flow and add missing packages to json

--- prisma/utils/client.ts
+++ prisma/utils/client.ts
@@ -1,5 +1,13 @@
 /** @format */
 
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
