16dcc58807a934ae3454190a48927012808c6663
Charuka deshan <itscharukadeshan@gmail.com>
2024-02-25 13:28:18 +0530

    config🔧: Fix the wrong omit id to userId

--- controllers/types.ts
+++ controllers/types.ts
@@ -1,5 +1,5 @@
 import { User, TwitchAuth, YouTubeAuth } from "@prisma/client";
 
 export type UserWithoutId = Omit<User, "id">;
-export type TwitchAuthWithoutId = Omit<TwitchAuth, "id">;
-export type YoutubeAuthWithoutId = Omit<YouTubeAuth, "id">;
+export type TwitchAuthWithoutId = Omit<TwitchAuth, "userId">;
+export type YoutubeAuthWithoutId = Omit<YouTubeAuth, "userId">;
