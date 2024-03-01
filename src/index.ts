2b3d8e1288b6ff16a9a31dabfa050c2e23334f9c
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-01 20:24:34 +0530

    format🥚: Add newRefreshToken

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,19 @@ import express, { Request, Response } from "express";
 import chalk from "chalk";
 import twitchRoutes from "./routes/twitch/twitch.routes";
 import youtubeRoutes from "./routes/youtube/youtube.routes";
+import { API_PORT } from "./config/config";
+import { authHandler } from "./middlewares/authHandler";
 const errorHandler = require("./middlewares/errorHandler");
 const requestLogger = require("./middlewares/requestLogger");
+const cookieParser = require("cookie-parser");
 
 const app: express.Application = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.use(cookieParser());
+
+app.get("/", authHandler, (req: Request, res: Response) => {
   res.send(`Welcome to clip thread api`);
 });
-
-const port = 3000;
 
 app.use(requestLogger);
 app.use(errorHandler);
@@ -21,6 +24,6 @@ app.use(errorHandler);
 app.use("/twitch", twitchRoutes);
 app.use("/youtube", youtubeRoutes);
 
-app.listen(port, () => {
+app.listen(API_PORT, () => {
   console.log(`${chalk.bgBlue.bold(" App is successfully deployed ! ")}`);
 });
