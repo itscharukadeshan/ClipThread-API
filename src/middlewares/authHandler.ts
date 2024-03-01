9047a7b7fb1de1fd225de742d93cf3c79cedff41
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-01 23:43:23 +0530

    feat✨: Add checks for authHandler

--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -1,18 +1,22 @@
 import { NextFunction, Request } from "express";
+
 const jwt = require("jsonwebtoken");
 import { ACCESS_TOKEN_SECRET } from "../config/config";
+import { getUser } from "../controllers/usersController";
+import { User } from "@prisma/client";
 
-export function authHandler(
+export async function authHandler(
   err: Error,
-  req: Request,
+  req: Request & { requiredRole?: string },
   res: any,
   next: NextFunction
 ) {
   try {
     const authHeader: string | undefined = req.headers.authorization;
+    const requiredRole: string | undefined = req.requiredRole;
 
     if (!authHeader) {
-      return res.status(401).json({ message: "Missing access Token !" });
+      return res.status(401).json({ message: "Missing access Token" });
     }
 
     const token: string = authHeader.split(" ")[1];
@@ -20,8 +24,22 @@ export function authHandler(
     const decodedToken = verifyToken(token, ACCESS_TOKEN_SECRET);
     const { userId, role } = decodedToken;
 
-    console.log(userId, role);
+    if (!userId || !role) {
+      return res.status(403).json({ message: "Invalid access token" });
+    }
 
+    const user: User = await getUser(userId);
+
+    if (!user) {
+      return res.status(403).json({ message: "Failed to find user" });
+    }
+    if (user.id !== userId) {
+      return res.status(403).json({ message: "Invalid user" });
+    }
+
+    if (user.login !== requiredRole) {
+      return res.status(403).json({ message: "You don't have access " });
+    }
     next();
   } catch (error: any) {
     if (error.message === "Token has expired") {
