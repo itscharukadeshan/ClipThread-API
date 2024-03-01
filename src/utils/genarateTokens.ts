394e93b14720fc078f2fc67646f7f284b2d96528
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-01 11:20:14 +0530

    file📦: Add generateToken util

---
+++ src/utils/genarateTokens.ts
@@ -0,0 +1,21 @@
+import { UserRole } from "@prisma/client";
+import {
+  AUTH_TOKEN_EXPIRATION,
+  REFRESH_TOKEN_EXPIRATION,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} from "../config/config";
+const jwt = require("jsonwebtoken");
+
+export function generateAuthToken(userId: string, role: UserRole) {
+  return jwt.sign({ userId: userId, role: UserRole }, ACCESS_TOKEN_SECRET, {
+    expiresIn: AUTH_TOKEN_EXPIRATION,
+  });
+}
+
+export function generateRefreshToken() {
+  const refreshToken = jwt.sign({}, REFRESH_TOKEN_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRATION,
+  });
+  return refreshToken;
+}
