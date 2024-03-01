44fcf1d9e97dbe4e4ed579de91485c43b18ed0a0
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-01 12:31:54 +0530

    feat✨: Add handleUserScope ( )  , handleModeratorScope ( ) and handleCreatorScope (  ) functions

---
+++ src/utils/authUtils.ts
@@ -0,0 +1,77 @@
+import { createUser, updateUser } from "../controllers/usersController";
+import {
+  getBlockedUsers,
+  getModeratedChannels,
+} from "../services/twitch/twitchAuth.service";
+import { generateAccessToken, generateRefreshToken } from "./generateTokens";
+import { TwitchAuthWithoutId, UserWithoutId } from "./types";
+
+export const handleUserScope = async (
+  userData: UserWithoutId,
+  twitchAuth: TwitchAuthWithoutId,
+  accessToken: string
+) => {
+  let newUser, blockedUsers, user, newRefreshToken, newAccessToken;
+
+  newUser = await createUser(userData, twitchAuth);
+  blockedUsers = await getBlockedUsers(accessToken, newUser.twitchId as string);
+  newAccessToken = generateAccessToken(newUser.id, newUser.login);
+  newRefreshToken = generateRefreshToken();
+
+  user = await updateUser(newUser.id, {
+    blockedUsers,
+    refreshToken: newRefreshToken,
+  });
+
+  return { user, newAccessToken };
+};
+
+export const handleModeratorScope = async (
+  userData: UserWithoutId,
+  twitchAuth: TwitchAuthWithoutId,
+  accessToken: string
+) => {
+  let newUser,
+    blockedUsers,
+    moderatedChannels,
+    user,
+    newRefreshToken,
+    newAccessToken;
+
+  newUser = await createUser(userData, twitchAuth);
+  blockedUsers = await getBlockedUsers(accessToken, newUser.twitchId as string);
+  moderatedChannels = await getModeratedChannels(
+    accessToken,
+    newUser.twitchId as string
+  );
+  newAccessToken = generateAccessToken(newUser.id, newUser.login);
+  newRefreshToken = generateRefreshToken();
+
+  user = await updateUser(newUser.id, {
+    blockedUsers,
+    moderatedChannels,
+    refreshToken: newRefreshToken,
+  });
+
+  return { user, newAccessToken };
+};
+
+export const handleCreatorScope = async (
+  userData: UserWithoutId,
+  twitchAuth: TwitchAuthWithoutId,
+  accessToken: string
+) => {
+  let newUser, blockedUsers, user, newRefreshToken, newAccessToken;
+
+  newUser = await createUser(userData, twitchAuth);
+  blockedUsers = await getBlockedUsers(accessToken, newUser.twitchId as string);
+  newAccessToken = generateAccessToken(newUser.id, newUser.login);
+  newRefreshToken = generateRefreshToken();
+
+  user = await updateUser(newUser.id, {
+    blockedUsers,
+    refreshToken: newRefreshToken,
+  });
+
+  return { user, newAccessToken };
+};
