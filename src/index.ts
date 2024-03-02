57a3e331043cd25d6d73a8456630edb1bff5b8ee
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-02 09:54:40 +0530

    format🥚: Minor style and syntax changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import chalk from "chalk";
 import twitchRoutes from "./routes/twitch/twitch.routes";
 import youtubeRoutes from "./routes/youtube/youtube.routes";
 import { API_PORT } from "./config/config";
-import { authHandler } from "./middlewares/authHandler";
+
+const authHandler = require("./middlewares/authHandler");
 const errorHandler = require("./middlewares/errorHandler");
 const requestLogger = require("./middlewares/requestLogger");
 const cookieParser = require("cookie-parser");
@@ -14,12 +15,12 @@ const app: express.Application = express();
 
 app.use(cookieParser());
 
+app.use(requestLogger);
+app.use(errorHandler);
+
 app.get("/", authHandler, (req: Request, res: Response) => {
   res.send(`Welcome to clip thread api`);
 });
-
-app.use(requestLogger);
-app.use(errorHandler);
 
 app.use("/twitch", twitchRoutes);
 app.use("/youtube", youtubeRoutes);
--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -5,15 +5,14 @@ import { ACCESS_TOKEN_SECRET } from "../config/config";
 import { getUser } from "../controllers/usersController";
 import { User } from "@prisma/client";
 
-export async function authHandler(
+async function authHandler(
   err: Error,
-  req: Request & { requiredRole?: string },
+  req: Request,
   res: any,
   next: NextFunction
 ) {
   try {
     const authHeader: string | undefined = req.headers.authorization;
-    const requiredRole: string | undefined = req.requiredRole;
 
     if (!authHeader) {
       return res.status(401).json({ message: "Missing access Token" });
@@ -22,7 +21,7 @@ export async function authHandler(
     const token: string = authHeader.split(" ")[1];
 
     const decodedToken = verifyToken(token, ACCESS_TOKEN_SECRET);
-    const { userId, role } = decodedToken;
+    const { payload: userId, role } = decodedToken;
 
     if (!userId || !role) {
       return res.status(403).json({ message: "Invalid access token" });
@@ -37,9 +36,8 @@ export async function authHandler(
       return res.status(403).json({ message: "Invalid user" });
     }
 
-    if (user.login !== requiredRole) {
-      return res.status(403).json({ message: "You don't have access " });
-    }
+    console.log({ userId, role });
+
     next();
   } catch (error: any) {
     if (error.message === "Token has expired") {
@@ -58,3 +56,5 @@ function verifyToken(token: string, secretKey: string) {
     return null;
   }
 }
+
+module.exports = authHandler;
