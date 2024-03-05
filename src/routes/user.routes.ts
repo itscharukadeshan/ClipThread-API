ad8d9f014495127b4314270f5171579a6624a539
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-05 16:59:42 +0530

    file📦: Add routes files for thread and user with basic structure

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,9 @@ import { Router, Response, Request, NextFunction } from "express";
 
 const router = Router();
 
-router.get(
-  "/treading",
+router.post(
+  "/profile-update",
   (req: Request, res: Response, next: NextFunction) => {}
 );
-
-router.get("/filter", (req: Request, res: Response, next: NextFunction) => {});
 
 export default router;
