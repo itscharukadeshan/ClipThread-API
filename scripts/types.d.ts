7d2d43165e51af30cace580e4bd2de1d6d945015
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-05 10:29:33 +0530

    format🥚: Add missing types

--- scripts/types.d.ts
+++ scripts/types.d.ts
@@ -8,3 +8,18 @@ export type UserWithAuth = User & {
   twitchAuth: TwitchAuth | null;
   youtubeAuth: YouTubeAuth | null;
 };
+
+export type AuthData = {
+  twitchAuthData: {
+    refreshToken: string;
+    accessToken: string;
+    expiryTime: Date;
+  } | null;
+  youtubeAuthData: {
+    refreshToken: string;
+    accessToken: string;
+    expiryTime: Date;
+  } | null;
+};
+
+export type FakeData = Array<{ userData: UserWithoutId; authData: AuthData }>;
