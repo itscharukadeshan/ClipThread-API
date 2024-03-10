a610a9d301aa416bd88722fd8845e456ea48b070
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-10 15:07:55 +0530

    feat✨: Update the endpoints

--- src/routes/thread.routes.ts
+++ src/routes/thread.routes.ts
@@ -3,10 +3,13 @@ import { Router, Response, Request, NextFunction } from "express";
 const router = Router();
 
 router.get(
-  "/treading",
+  "/trending",
   (req: Request, res: Response, next: NextFunction) => {}
 );
 
-router.get("/filter", (req: Request, res: Response, next: NextFunction) => {});
+router.get(
+  "/collections/:userId",
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
 export default router;
