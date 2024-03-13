c62ac738930ec540e9ced7f785cbb73a880c82e9
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-13 19:31:39 +0530

    feat✨: Implement thread/threadId endpoint

--- src/routes/thread.routes.ts
+++ src/routes/thread.routes.ts
@@ -1,15 +1,27 @@
 import { Router, Response, Request, NextFunction } from "express";
+import { Thread } from "@prisma/client";
+import { getPublicThreadDataById } from "../controllers/threadsController";
 
 const router = Router();
 
 router.get(
-  "/trending",
-  (req: Request, res: Response, next: NextFunction) => {}
-);
+  "/:threadId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const threadId = req.params.threadId;
 
-router.get(
-  "/collections/:userId",
-  (req: Request, res: Response, next: NextFunction) => {}
+    let thread: Partial<Thread> | null;
+
+    try {
+      thread = await getPublicThreadDataById(threadId);
+      if (thread === null) {
+        return res.status(400).json({ message: `Thread not found` });
+      }
+
+      return res.status(200).json({ thread });
+    } catch (error) {
+      return res.status(400).json({ message: `Something went wrong !` });
+    }
+  }
 );
 
 export default router;
