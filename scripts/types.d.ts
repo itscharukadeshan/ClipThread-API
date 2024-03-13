69bc2efb891f7f786d61186a8052b247c7dc4112
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-13 15:01:49 +0530

    feat✨: Add BroadcastersWithoutId types

--- scripts/types.d.ts
+++ scripts/types.d.ts
@@ -3,21 +3,25 @@ import {
   TwitchAuth,
   YouTubeAuth,
   Thread,
-  ThreadClip,
   Clip,
+  Broadcasters,
 } from "@prisma/client";
 
 export type UserWithoutId = Omit<User, "id">;
 export type TwitchAuthWithoutId = Omit<TwitchAuth, "userId">;
 export type YoutubeAuthWithoutId = Omit<YouTubeAuth, "userId">;
 export type ThreadWithoutId = Omit<Thread, "id">;
-export type ThreadClipWithoutId = Omit<ThreadClip, "id">;
+export type BroadcastersWithoutId = Omit<Broadcasters, "id">;
 export type ClipWithoutId = Omit<Clip, "id">;
 
 export type UserWithAuth = User & {
   twitchAuth: TwitchAuth | undefined;
   youtubeAuth: YouTubeAuth | undefined;
 };
+
+export type BroadcasterIds = {
+  id: string;
+}[];
 
 export type AuthData = {
   twitchAuthData:
