3ef83be92bb011d62dd4e2be0aff5126676d4c8e
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-16 10:38:52 +0530

    feat✨: Add post req route

--- src/routes/thread.routes.ts
+++ src/routes/thread.routes.ts
@@ -1,9 +1,14 @@
 import { Router, Response, Request, NextFunction } from "express";
-import { Prisma, Thread } from "@prisma/client";
+import { Thread } from "@prisma/client";
 import {
   getPublicThreadDataById,
   getThreadStatus,
+  createNewThread,
 } from "../controllers/threadsController";
+import authHandler from "../middlewares/authHandler";
+import { verifyToken } from "../utils/authUtils";
+import { ACCESS_TOKEN_SECRET } from "../config/config";
+import { TokenPayload } from "./types";
 
 const router = Router();
 
@@ -40,6 +45,51 @@ router.get(
       return res.status(400).json({ message: `Something went wrong !` });
     }
   }
+);
+
+router.post(
+  "/create",
+  authHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader: string | undefined = req.headers.authorization;
+    const title = req.params.title;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "Missing access Token" });
+    } else if (!title) {
+      return res.status(401).json({ message: "Missing thread tittle" });
+    }
+
+    const token: string = authHeader.split(" ")[1];
+
+    const decodedToken = verifyToken(token, ACCESS_TOKEN_SECRET);
+    const { userId, role } = decodedToken as TokenPayload;
+
+    let thread: Partial<Thread> | null;
+
+    try {
+      thread = await createNewThread(userId, title);
+      if (thread === null) {
+        return res.status(400).json({ message: `Something went wrong` });
+      }
+
+      return res.status(200).json({ thread });
+    } catch (error) {
+      return res.status(400).json({ message: `Something went wrong !` });
+    }
+  }
+);
+
+router.put(
+  "/update",
+  authHandler,
+  (req: Request, res: Response, next: NextFunction) => {}
+);
+
+router.delete(
+  "/:threadId",
+  authHandler,
+  (req: Request, res: Response, next: NextFunction) => {}
 );
 
 export default router;
