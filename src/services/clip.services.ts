1e40d68564d965adc922eaa9e777e8c6c5f4a66c
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-22 15:51:46 +0530

    feat✨: Add getYoutubeClipInfo ( ) req

--- src/services/clip.services.ts
+++ src/services/clip.services.ts
@@ -1,13 +1,14 @@
-import axios from "axios";
-import { TWITCH_CLIENT_ID } from "../config/config";
+import axios, { AxiosResponse } from "axios";
+import { TWITCH_CLIENT_ID, YOUTUBE_CLIENT_ID } from "../config/config";
 
-const CLIP_INFO_URL = "https://api.twitch.tv/helix/clips";
+const TWITCH_CLIP_INFO_URL = "https://api.twitch.tv/helix/clips";
+const YOUTUBE_CLIP_INFO_URL = "https://www.googleapis.com/youtube/v3/videos";
 
 export const getTwitchClipInfo = async (
   clipId: string,
   accessToken: string
 ) => {
-  const response = await axios.get(CLIP_INFO_URL, {
+  const response: AxiosResponse = await axios.get(TWITCH_CLIP_INFO_URL, {
     headers: {
       "Client-ID": TWITCH_CLIENT_ID,
       Authorization: `Bearer ${accessToken}`,
@@ -16,4 +17,10 @@ export const getTwitchClipInfo = async (
   });
   return response.data;
 };
-export const getYoutubeClipInfo = async (clipId: string) => {};
+export const getYoutubeClipInfo = async (clipId: string) => {
+  const response: AxiosResponse = await axios.get(YOUTUBE_CLIP_INFO_URL, {
+    params: { id: clipId, key: YOUTUBE_CLIENT_ID },
+  });
+
+  return response.data;
+};
