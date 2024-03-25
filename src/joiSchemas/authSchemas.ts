51cb6481194246523c6a0dc3e92cb97648925fdf
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-25 15:21:09 +0530

    feat✨: Add schema check for error

---
+++ src/joiSchemas/authSchemas.ts
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const refreshTokenSchema = Joi.object({
+  refresh_token: Joi.string()
+    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/)
+    .required(),
+});
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import { REFRESH_TOKEN_SECRET } from "../config/config";
 import { getUserByRefreshToken } from "../controllers/usersController";
 import { generateAccessToken } from "../utils/generateTokens";
 import revokedTokenHandler from "../middlewares/revokedTokenHandler";
+import { refreshTokenSchema } from "../joiSchemas/authSchemas";
 
 const router = Router();
 
@@ -11,27 +12,37 @@ router.get(
   "/access-token",
   revokedTokenHandler,
   async (req: Request, res: Response, next: NextFunction) => {
-    const refreshToken: string = req.cookies.refresh_token;
+    try {
+      const { error } = refreshTokenSchema.validate(req.cookies);
 
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Missing refresh token" });
+      if (error) {
+        return res.status(400).json({ message: error.message });
+      }
+
+      const refreshToken: string = req.cookies.refresh_token;
+
+      if (!refreshToken) {
+        return res.status(401).json({ message: "Missing refresh token" });
+      }
+
+      if (!verifyRefreshToken(refreshToken, REFRESH_TOKEN_SECRET)) {
+        return res.status(401).json({ message: "Invalid refresh token" });
+      }
+
+      const user = await getUserByRefreshToken(refreshToken);
+
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Invalid refresh token : Unable to find user" });
+      }
+
+      const accessToken = generateAccessToken(user.id, user.login);
+
+      return res.status(200).json({ access_token: `'Bearer ${accessToken}` });
+    } catch (error) {
+      next(error);
     }
-
-    if (!verifyRefreshToken(refreshToken, REFRESH_TOKEN_SECRET)) {
-      return res.status(401).json({ message: "Invalid refresh token" });
-    }
-
-    const user = await getUserByRefreshToken(refreshToken);
-
-    if (!user) {
-      return res
-        .status(401)
-        .json({ message: "Invalid refresh token : Unable to find user" });
-    }
-
-    const accessToken = generateAccessToken(user.id, user.login);
-
-    return res.status(200).json({ access_token: `'Bearer ${accessToken}` });
   }
 );
 
