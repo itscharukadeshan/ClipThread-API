81b9d7c6fc3f849b5b740b54d6f9b6e3034f00e1
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-26 13:31:06 +0530

    feat✨: Add threadSchema  and clipSchema

--- src/joi_schemas/threadSchemas.ts
+++ src/joi_schemas/threadSchemas.ts
@@ -1,2 +1,39 @@
 import Joi from "joi";
-export const threadSchemas = Joi.object();
+
+const clipSchema = Joi.object({
+  id: Joi.string().required(),
+  order: Joi.number().integer().default(1),
+  clipId: Joi.string().required(),
+  creatorName: Joi.string().required(),
+  creatorId: Joi.string().required(),
+  broadcasterId: Joi.string().required(),
+  broadcasterName: Joi.string().required(),
+  gameId: Joi.string().required(),
+  viewCount: Joi.number().integer().required(),
+  thumbUrl: Joi.string().required(),
+  embedUrl: Joi.string().required(),
+  description: Joi.string().allow(null).optional(),
+  url: Joi.string().required(),
+  tagId: Joi.string().default(""),
+});
+
+export const threadSchema = Joi.object({
+  id: Joi.string().required(),
+  title: Joi.string().required(),
+  description: Joi.string().allow(null).optional(),
+  published: Joi.boolean().default(false),
+  publishedTime: Joi.date().iso().default(Date.now),
+  createdAt: Joi.date().iso().default(Date.now),
+  updatedAt: Joi.date().iso().optional().default(null),
+  authorId: Joi.string().required(),
+  broadcasters: Joi.array().items(
+    Joi.object({
+      id: Joi.string().required(),
+      name: Joi.string().required(),
+      profileUrl: Joi.string().uri().required(),
+      platform: Joi.string().required(),
+      profilePic: Joi.string().uri().required(),
+    })
+  ),
+  clips: Joi.array().items(clipSchema),
+});
