2727cbd55ef6412a8b938eaab931cc13a464a2ed
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-26 13:25:05 +0530

    feat✨: Add accessTokenSchema

--- src/joi_schemas/authSchemas.ts
+++ src/joi_schemas/authSchemas.ts
@@ -1,8 +1,15 @@
 import Joi from "joi";
+
 export const refreshTokenSchema = Joi.object({
   refresh_token: Joi.string()
     .required()
-    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/),
+    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/)
+    .error(new Error("Invalid or missing refresh token")),
 });
+export const accessTokenSchema = Joi.string()
+  .trim()
+  .pattern(/^(Bearer|bearer) [a-zA-Z0-9\\-_]+$/)
+  .required()
+  .error(new Error("Invalid or missing access token"));
 
 export const authSchemas = Joi.object({});
