4655f9c61b86ff488d3694b268ad9865f26846f7
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-28 00:08:59 +0530

    format🥚: Remove unwanted console logs

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -13,13 +13,12 @@ if (!fs.existsSync(logDirectory)) {
 }
 
 const logger = winston.createLogger({
-  level: "request-info",
+  level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.json()
   ),
   transports: [
-    new winston.transports.Console(),
     new winston.transports.File({
       filename: logFilePath,
       maxsize: 1024 * 1024 * 1024,
