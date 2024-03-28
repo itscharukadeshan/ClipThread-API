6e495205d6ca25a2e55f833ae92b76d93be4f1b6
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-28 12:56:21 +0530

    feat✨: Implement application error

--- src/services/clip.services.ts
+++ src/services/clip.services.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { TWITCH_CLIENT_ID, YOUTUBE_CLIENT_ID } from "../config/config";
+import ApplicationError from "../errors/applicationError";
 
 const TWITCH_CLIP_INFO_URL = "https://api.twitch.tv/helix/clips";
 const YOUTUBE_CLIP_INFO_URL = "https://www.googleapis.com/youtube/v3/videos";
@@ -8,19 +9,28 @@ export const getTwitchClipInfo = async (
   clipId: string,
   accessToken: string
 ) => {
-  const response: AxiosResponse = await axios.get(TWITCH_CLIP_INFO_URL, {
-    headers: {
-      "Client-ID": TWITCH_CLIENT_ID,
-      Authorization: `Bearer ${accessToken}`,
-    },
-    params: { id: clipId },
-  });
-  return response.data;
+  try {
+    const response: AxiosResponse = await axios.get(TWITCH_CLIP_INFO_URL, {
+      headers: {
+        "Client-ID": TWITCH_CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: { id: clipId },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 export const getYoutubeClipInfo = async (clipId: string) => {
-  const response: AxiosResponse = await axios.get(YOUTUBE_CLIP_INFO_URL, {
-    params: { id: clipId, key: YOUTUBE_CLIENT_ID },
-  });
+  try {
+    const response: AxiosResponse = await axios.get(YOUTUBE_CLIP_INFO_URL, {
+      params: { id: clipId, key: YOUTUBE_CLIENT_ID },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
