a20323ce4547bdd0446b5cf7bb336fbfd0be7ece
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-28 12:56:02 +0530

    feat✨: Add error console log and handle prisma errors

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import ApplicationError from "../errors/applicationError";
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
+import chalk from "chalk";
+
 function errorHandler(
   err: Error,
   req: Request,
@@ -8,9 +16,21 @@ function errorHandler(
 ) {
   if (err instanceof ApplicationError) {
     res.status(err.statusCode).json({ error: err.message });
-  } else {
-    console.error("Unhandled error:", err);
+  } else if (
+    err instanceof PrismaClientInitializationError ||
+    err instanceof PrismaClientKnownRequestError ||
+    err instanceof PrismaClientUnknownRequestError ||
+    err instanceof PrismaClientValidationError
+  ) {
+    if (process.env.NODE_ENV === "development") {
+      console.error(`${chalk.red(err.message)} => ${chalk.yellow(err.stack)}`);
+    }
     res.status(500).json({ error: "Internal Server Error" });
+  } else {
+    if (process.env.NODE_ENV === "development") {
+      console.error(`${chalk.red(err.message)} => ${chalk.yellow(err.stack)}`);
+    }
+    res.status(500).json({ error: "Internal Server Error : Undefined" });
   }
 }
 
