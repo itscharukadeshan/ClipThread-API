af072257f1c22a04eb0bd4ce3de90e7367c86681
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-28 13:44:42 +0530

    feat✨: Remove unwanted errors from schema

--- src/joi_schemas/authSchemas.ts
+++ src/joi_schemas/authSchemas.ts
@@ -3,14 +3,12 @@ import Joi from "joi";
 export const refreshTokenSchema = Joi.object({
   refresh_token: Joi.string()
     .required()
-    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/)
-    .error(new Error("Invalid or missing refresh token")),
+    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/),
 });
 export const accessTokenSchema = Joi.string()
   .trim()
   .pattern(/^(Bearer|bearer) [a-zA-Z0-9\\-_]+$/)
-  .required()
-  .error(new Error("Invalid or missing access token"));
+  .required();
 
 export const querySchema = Joi.object({
   code: Joi.string().required(),
