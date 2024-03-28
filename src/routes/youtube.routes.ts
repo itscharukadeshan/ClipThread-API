ceb8fe7c523bd275d77a8c60ace8f0131a55ef81
Charuka deshan <itscharukadeshan@gmail.com>
2024-03-28 13:45:05 +0530

    refactor🎨: Remove querySchema for auth

--- src/routes/youtube.routes.ts
+++ src/routes/youtube.routes.ts
@@ -9,8 +9,6 @@ import {
 
 import { formatUserDataFromYouTube } from "../utils/formatUserData";
 
-import { querySchema } from "../joi_schemas/authSchemas";
-
 import handelYoutubeUser from "../utils/handleYoutubeUser";
 import ApplicationError from "../errors/applicationError";
 
@@ -28,53 +26,51 @@ router.get(
   "/callback",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const query = req.query;
-      const { error } = querySchema.validate(query);
+      const code = req.query.code as string;
 
-      if (error) {
-        throw new ApplicationError(error.message, 400);
-      }
+      let authData = null;
 
-      let authData;
-
-      if (query.code) {
-        const code = query.code as string;
+      if (code) {
         authData = await getAccessToken(code);
+      } else {
+        throw new ApplicationError("Missing scope", 400);
       }
 
-      const userDataResponse = await getUser(authData.access_token);
-      const channelData = await getChannelData(authData.access_token);
-      const { userData, youtubeAuth } = formatUserDataFromYouTube(
-        authData,
-        userDataResponse,
-        channelData
-      );
-      const scope = userData.login;
+      if (authData) {
+        const userDataResponse = await getUser(authData.access_token);
+        const channelData = await getChannelData(authData.access_token);
+        const { userData, youtubeAuth } = formatUserDataFromYouTube(
+          authData,
+          userDataResponse,
+          channelData
+        );
+        const scope = userData.login;
 
-      const { newAccessToken, user } = await handelYoutubeUser(
-        userData,
-        youtubeAuth,
-        scope
-      );
+        const { newAccessToken, user } = await handelYoutubeUser(
+          userData,
+          youtubeAuth,
+          scope
+        );
 
-      const newRefreshToken = user?.refreshToken;
+        const newRefreshToken = user?.refreshToken;
 
-      res.cookie("refresh_token", newRefreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 2592000000,
-      });
+        res.cookie("refresh_token", newRefreshToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
+          maxAge: 2592000000,
+        });
 
-      res.status(200).json({
-        access_token: `Bearer ${newAccessToken}`,
-        username: user?.displayName,
-        userId: user?.id,
-        profileImage: user?.profileImageUrl,
-        youtubeId: user?.youtubeId,
-        role: user?.login,
-        followers: user?.followers,
-      });
+        res.status(200).json({
+          access_token: `Bearer ${newAccessToken}`,
+          username: user?.displayName,
+          userId: user?.id,
+          profileImage: user?.profileImageUrl,
+          youtubeId: user?.youtubeId,
+          role: user?.login,
+          followers: user?.followers,
+        });
+      }
     } catch (error) {
       next(error);
     }
