5b130497f334bda1660d75370f34058865dcaaba
Charuka deshan <itscharukadeshan@gmail.com>
2024-04-01 15:57:45 +0530

    fix🐛: Fix the access token flow

--- src/controllers/clipsControllers.ts
+++ src/controllers/clipsControllers.ts
@@ -19,36 +19,37 @@ export async function getTwitchAccessTokenById(userId: string) {
     }
 
     const expiryTime = twitchAuth?.expiryTime;
-    const isExpired: boolean = moment(expiryTime).isBefore(moment());
+    const isExpired: boolean = moment(expiryTime).isAfter(moment());
 
-    if (isExpired) {
-      if (!twitchAuth?.refreshToken) {
-        return null;
-      }
-    }
     const refreshToken = decryptData(twitchAuth?.refreshToken as string);
 
-    const refreshTokenResponse: RefreshTokenResponse =
-      await getTwitchAccessTokenByRefToken(refreshToken);
+    if (isExpired) {
+      const refreshTokenResponse: RefreshTokenResponse =
+        await getTwitchAccessTokenByRefToken(refreshToken);
 
-    if (!refreshTokenResponse) {
-      throw new ApplicationError("Failed to refresh twitch access token", 500);
+      if (!refreshTokenResponse) {
+        throw new ApplicationError(
+          "Failed to refresh twitch access token",
+          500
+        );
+      }
+      const updatedTwitchAuth = await prisma.twitchAuth.update({
+        where: { userId },
+        data: {
+          accessToken: refreshTokenResponse.access_token,
+          refreshToken: refreshTokenResponse.refresh_token,
+          expiryTime: moment().add(3.5, "hours").toString(),
+        },
+      });
+
+      if (!updatedTwitchAuth) {
+        return null;
+      }
+
+      return updatedTwitchAuth.accessToken;
+    } else {
+      return refreshToken;
     }
-
-    const updatedTwitchAuth = await prisma.twitchAuth.update({
-      where: { userId },
-      data: {
-        accessToken: refreshTokenResponse.access_token,
-        refreshToken: refreshTokenResponse.refresh_token,
-        expiryTime: moment().add(3.5, "hours").toString(),
-      },
-    });
-
-    if (!updatedTwitchAuth) {
-      return null;
-    }
-
-    return updatedTwitchAuth.accessToken;
   } catch (error) {
     throw error;
   }
