f49a355b96752e8464bea5efb486f020482fe6d8
Charuka deshan <itscharukadeshan@gmail.com>
2024-04-02 12:43:50 +0530

    feat✨: Remove schema regex check

--- src/joi_schemas/authSchemas.ts
+++ src/joi_schemas/authSchemas.ts
@@ -1,9 +1,7 @@
 import Joi from "joi";
 
 export const refreshTokenSchema = Joi.object({
-  refresh_token: Joi.string()
-    .required()
-    .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/),
+  refresh_token: Joi.string().required(),
 });
 export const accessTokenSchema = Joi.string().required();
 
