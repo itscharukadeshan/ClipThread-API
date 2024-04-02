83e3b78999a56ef2a39e8729b47e855f161e185c
Charuka deshan <itscharukadeshan@gmail.com>
2024-04-02 12:02:28 +0530

    refactor🎨: Remove regex from the check

--- src/joi_schemas/authSchemas.ts
+++ src/joi_schemas/authSchemas.ts
@@ -5,10 +5,7 @@ export const refreshTokenSchema = Joi.object({
     .required()
     .pattern(/^([a-zA-Z0-9\-_]+\.){2}[a-zA-Z0-9\-_]+$/),
 });
-export const accessTokenSchema = Joi.string()
-  .trim()
-  .pattern(/^(Bearer|bearer) [a-zA-Z0-9\\-_]+$/)
-  .required();
+export const accessTokenSchema = Joi.string().required();
 
 export const querySchema = Joi.object({
   code: Joi.string().required(),
