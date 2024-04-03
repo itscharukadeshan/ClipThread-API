0547353135d0533bb1a67b2539f5edf3d8de2f7c
Charuka deshan <itscharukadeshan@gmail.com>
2024-04-03 12:59:28 +0530

    feat✨: Add getTwitchClipIdFromUrl and getYoutubeIdFromUrl

--- src/utils/checkUrlOrigin.ts
+++ src/utils/checkUrlOrigin.ts
@@ -1,20 +1,116 @@
 export const checkUrlOrigin = async (url: string) => {
-  const twitchPattern =
-    /^https?:\/\/(?:www\.)?twitch\.tv\/\w+\/clip\/([a-zA-Z0-9-]+)/;
-  const youtubePattern =
-    /^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9-_]+)/;
+  let uri: URL;
+  try {
+    uri = new URL(url);
+  } catch {
+    return undefined;
+  }
+
+  const twitchPattern = /^(clips\.twitch\.tv|.*twitch\.tv\/clip\/)/;
+  const youtubePattern = /^(youtu\.be|.*youtube\.com\/shorts|.*youtube\.com$)/;
 
   const isTwitchUrl: RegExpMatchArray | null = url.match(twitchPattern);
 
   if (isTwitchUrl) {
-    return ["Twitch", isTwitchUrl[1]];
+    const urlData = getTwitchClipIdFromUrl(url);
+    return urlData;
   }
 
   const isYoutubeUrl: RegExpMatchArray | null = url.match(youtubePattern);
 
   if (isYoutubeUrl) {
-    return ["YouTube", isYoutubeUrl[1]];
+    const urlData = getYoutubeIdFromUrl(url);
+    return urlData;
   }
 
-  return ["Invalid", ""];
+  return ["Invalid", undefined, undefined];
 };
+
+// extractIdFromPathname from the url and provide startTime if there
+
+function getYoutubeIdFromUrl(url: string): string[] | undefined {
+  let uri: URL;
+  try {
+    uri = new URL(url);
+  } catch {
+    return undefined;
+  }
+
+  let id: string | undefined = undefined;
+  if (uri.hostname === "youtu.be" || uri.pathname.includes("shorts")) {
+    const idStart = uri.pathname.lastIndexOf("/") + 1;
+    id = uri.pathname.slice(idStart).split("?")[0];
+  } else if (uri.hostname.endsWith("youtube.com")) {
+    id = uri.searchParams.get("v") ?? undefined;
+  }
+
+  if (!id) {
+    return undefined;
+  }
+
+  const startTime = uri.searchParams.get("t") ?? undefined;
+
+  if (startTime) {
+    const chunks = startTime.split(/([hms])/).filter((chunk) => chunk !== "");
+    const magnitudes = chunks
+      .filter((chunk) => chunk.match(/[0-9]+/))
+      .map((chunk) => parseInt(chunk));
+    const UNITS = ["h", "m", "s"];
+    const seenUnits = chunks.filter((chunk) => UNITS.includes(chunk));
+
+    if (chunks.length === 1) {
+      return ["YouTube", id, chunks[0]];
+    } else {
+      const normalizedStartTime = magnitudes.reduce(
+        (accum, magnitude, index) => {
+          let conversionFactor = 0;
+
+          if (seenUnits[index] === "h") {
+            conversionFactor = 3600;
+          } else if (seenUnits[index] === "m") {
+            conversionFactor = 60;
+          } else if (seenUnits[index] === "s") {
+            conversionFactor = 1;
+          }
+
+          return accum + magnitude * conversionFactor;
+        },
+        0
+      );
+
+      return ["YouTube", id, `${normalizedStartTime}`];
+    }
+  } else {
+    return ["YouTube", id];
+  }
+}
+
+// extractIdFromPathname from url
+
+function getTwitchClipIdFromUrl(url: string): string[] | undefined {
+  let uri: URL;
+  try {
+    uri = new URL(url);
+  } catch {
+    return undefined;
+  }
+
+  function extractIdFromPathname(pathname: string): string | undefined {
+    const idStart = pathname.lastIndexOf("/");
+    const id = pathname.slice(idStart).split("?")[0].slice(1);
+
+    return id;
+  }
+
+  if (uri.hostname === "clips.twitch.tv") {
+    return ["Twitch", `${extractIdFromPathname(uri.pathname)}`];
+  }
+
+  if (uri.hostname.endsWith("twitch.tv")) {
+    if (uri.pathname.includes("/clip/")) {
+      return ["Twitch", `${extractIdFromPathname(uri.pathname)}`];
+    }
+  }
+
+  return undefined;
+}
