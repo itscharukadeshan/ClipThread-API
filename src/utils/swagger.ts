985871773dd47e096ab1b263656239449fac2151
charuka deshan <itscharukadeshan@gmail.com>
2024-06-22 13:51:57 +0530

    config🔧: Add swagger basic config

---
+++ src/utils/swagger.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+import { Express, Request, Response } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { version } from "../../package.json";
