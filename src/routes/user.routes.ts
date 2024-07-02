74df568770e1d9d3b55f578bfe5f10f50e388715
charuka deshan <itscharukadeshan@gmail.com>
2024-07-02 15:08:03 +0530

    docs📝: Add userId docs

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Router, Response, Request, NextFunction } from "express";
 import { getPublicUserDataById } from "../controllers/usersController";
 import { User } from "@prisma/client";
@@ -5,6 +7,52 @@ import { userIdSchema } from "../joi_schemas/userSchemas";
 import ApplicationError from "../errors/applicationError";
 
 const router = Router();
+
+/**
+ * @openapi
+ * /user/{userId}:
+ *   get:
+ *     summary: Get public user info by userId
+ *     tags:
+ *       - User
+ *     description: Get public user data using the userId in the path.
+ *     security: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         description: Valid user id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Send public user data with status
+ *         content:
+ *           application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Missing, Invalid or not found threadId information.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApplicationError'
+ *
+ *       404:
+ *         description: Thread data not found / not published
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApplicationError'
+ *
+ *       500:
+ *         description:  Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApplicationError'
+ *
+ */
 
 router.get(
   "/:userId",
